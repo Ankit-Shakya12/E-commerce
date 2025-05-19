58d1bdd241ff4e4f09f49c28c92717ead3599c3c
Ankit-Shakya12 <ankitshakya0210@gmail.com>
2025-05-19 10:16:04 +0530

    Created bazarstore app

---
+++ Bazaari/bazarstore/urls.py
@@ -0,0 +1,8 @@
+from django.urls import path
+from . import views
+
+urlpatterns = [
+    path('', views.index,name='home'),
+    path('login/',views.login_page,name='login'),
+    path('register/',views.register_page,name='register'),
+]
